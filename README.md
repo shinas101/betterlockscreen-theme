6ac41ee8d6a626ce89f77906da3a146eac564c2d
shinas101 <shinassamooncvd@gmail.com>
2022-06-13 11:14:29 +0530

    New features

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 **MOONKNIGHT** theme for  betterlockscreen
 
-## preview :
-![preview](preview.png)
+## preview-Blur:
+![moonknight](/preview/moonknight-preview.png)
+![baruto](/preview/baruto-preview.png)
+![solo-leveling](/preview/solo-leveling-preview.png)
+
 
 ## Installation :
 clone the repo and run install.sh
@@ -11,5 +14,20 @@ clone the repo and run install.sh
 git clone https://github.com/shinas101/betterlockscreen-theme.git
 cd betterlockscreen-theme
 chmod +x install.sh
-./install.sh
+./install.sh -h # h for help
+			 -b # b for blur
+			 -n # n fot noblur
+			 
+	eg: ./install.sh -n
+		
+		Please select your theme (No-blur) :
+		1) Moonknight-noblur
+		2) Baruto-noblur
+		3) Solo-leveling-noblur
+		4) exit
+		#?  
 ```
+## preview-noblur:
+![moonkight-noblur](/preview/moonknight-noblur-preview.png)
+![baruto-noblur](/preview/baruto-noblur-preview.png)
+![solo-leveling-noblur](/preview/solo-leveling-noblur-preview.png)
--- install.sh
+++ install.sh
@@ -1,3 +1,87 @@
 #!/bin/bash
 
-betterlockscreen -u background.jpg
+function setWall() { 
+	printf "Please select your theme (blur) : \n"
+	options=("Moonknight" "Baruto" "Solo-leveling" "exit")
+   select opt in "${options[@]}";do
+		case $opt in
+
+			"Moonknight")
+				betterlockscreen -u $PWD/backgrounds/moonknight.jpg
+				break
+				;;
+			"Baruto")
+				betterlockscreen -u $PWD/backgrounds/baruto.jpg
+				break
+				;;
+			"Solo-leveling")
+				betterlockscreen -u $PWD/backgrounds/solo-leveling.jpg
+				break
+				;;
+			"exit")
+				break
+				;;
+			*)
+				printf  "Choose a theme!!\n"
+				;;
+		esac
+	done
+}
+function setWall_noBlurr() { 
+	printf "Please select your theme (No-blur) : \n"
+	options=("Moonknight-noblur" "Baruto-noblur" "Solo-leveling-noblur" "exit")
+   select opt in "${options[@]}";do
+		case $opt in
+
+			"Moonknight-noblur")
+				betterlockscreen -u $PWD/backgrounds/moonknight-noblur.jpg
+				break
+				;;
+			"Baruto-noblur")
+				betterlockscreen -u $PWD/backgrounds/baruto-noblur.png
+				break
+				;;
+			"Solo-leveling-noblur")
+				betterlockscreen -u $PWD/backgrounds/solo-leveling-noblur.png
+				break
+				;;
+			"exit")
+				break
+				;;
+			*)
+				printf  "Choose a theme!!\n"
+				;;
+		esac
+	done
+}
+	
+usage(){
+	printf "usage ./install.sh <options eg: -b , -h , -n ...> \noptions :  \n b - blurred \n h - help\n n - no blur\n"
+}
+
+while getopts "bhn" arg;do
+	case "${arg}" in
+		b)
+			setWall
+			exit 0
+			;;
+		n)
+			setWall_noBlurr
+			exit 0
+			;;
+		h)
+			usage
+			exit 0 
+			;;
+		*)
+			usage
+			break
+			;;
+	esac
+	
+done			
+
+
+
+
+
